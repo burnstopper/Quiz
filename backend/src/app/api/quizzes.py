3c2c97b1b750c863fbf1ccfbfce8c895d3290a5e
tlidzhiev <trlidzhiev@edu.hse.ru>
2023-04-04 18:53:44 +0300

    Fix quizzes crud and endpoints

--- backend/src/app/api/quizzes.py
+++ backend/src/app/api/quizzes.py
@@ -1,64 +1,103 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, func
 
 from app.crud.quiz import crud as crud_quizzes
-from app.crud.quiz_respondents import crud as crud_quiz_respondent
+from app.crud.quiz_respondents import crud as crud_quiz_respondents
 from app.database.dependencies import get_db
-from app.schemas.quiz import QuizCreate, Quiz, QuizUpdate
+from app.schemas.quiz import QuizCreate, QuizUpdate
+from app.schemas.quiz import Quiz as RequestedQuiz
+from app.models.quiz import Quiz
+
+
+async def check_is_quiz_name_unique(quiz_name: str, db: AsyncSession) -> bool:
+    query = (
+        select(Quiz.name)
+        .where(Quiz.name == quiz_name.strip())
+    )
+
+    is_used_quiz_name: str | None = (await db.execute(query)).first()
+    if is_used_quiz_name is not None:
+        return False
+    return True
+
+
+async def check_there_are_conflicts_with_other_names(quiz_id: int, quiz_name: str, db: AsyncSession) -> bool:
+    query = (
+        select(Quiz.id)
+        .where(Quiz.name == quiz_name.strip())
+    )
+
+    result_id: int = (await db.execute(query)).scalar()
+    if result_id is None:
+        return True
+    elif result_id != quiz_id:
+        return False
+    return True
+
+
+async def check_quiz_id_is_valid(quiz_id: int, db: AsyncSession) -> bool:
+    query = (
+        func.max(Quiz.id)
+    )
+
+    max_id: int = (await db.execute(query)).scalar()
+    if max_id is None:
+        max_id = 0
+    return quiz_id <= max_id
+
 
 router = APIRouter()
 
 
-@router.post('/', status_code=status.HTTP_201_CREATED)
-async def create_quiz(quiz_in: QuizCreate, db: AsyncSession = Depends(get_db)):
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=RequestedQuiz)
+async def create_quiz(quiz_in: QuizCreate, db: AsyncSession = Depends(get_db)) -> RequestedQuiz:
     """
     Create new quiz
     """
 
-    error: None | str = await crud_quizzes.create_new_quiz(db=db, quiz_in=quiz_in)
-    if error is not None:
-        raise HTTPException(status_code=409, detail=error)
+    if not check_is_quiz_name_unique(quiz_name=quiz_in.name, db=db):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail=f'Quiz with this name has already been created')
+
+    return await crud_quizzes.create_new_quiz(quiz_in=quiz_in, db=db)
 
 
-@router.put('/{quiz_id}', status_code=status.HTTP_200_OK)
-async def update_quiz(quiz_id: int, quiz_in: QuizUpdate, db: AsyncSession = Depends(get_db)):
+@router.put('/{quiz_id}', status_code=status.HTTP_200_OK, response_model=RequestedQuiz)
+async def update_quiz(quiz_id: int, quiz_in: QuizUpdate, db: AsyncSession = Depends(get_db)) -> RequestedQuiz:
     """
     Update quiz by id
     """
 
-    error: None | str = await crud_quizzes.update_quiz(db=db, quiz_id=quiz_id, quiz_in=quiz_in)
-    if error is not None:
-        raise HTTPException(status_code=409, detail=error)
+    if check_quiz_id_is_valid(quiz_id=quiz_id, db=db):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Quiz with this id has does not exist')
+
+    if check_there_are_conflicts_with_other_names(quiz_id=quiz_id, quiz_name=quiz_in.name, db=db):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail=f'Quiz with this name has already been created')
+
+    return await crud_quizzes.update_quiz(quiz_id=quiz_id, quiz_in=quiz_in, db=db)
 
 
-@router.get('/', status_code=status.HTTP_200_OK, response_model=list[Quiz])
-async def get_quizzes(respondent_id: int = None, db: AsyncSession = Depends(get_db)) -> list[Quiz]:
+@router.get('/', status_code=status.HTTP_200_OK, response_model=list[RequestedQuiz])
+async def get_all_quizzes(db: AsyncSession = Depends(get_db)) -> list[RequestedQuiz]:
     """
-    Get all quizzes or quizzes for the respondent
+    Get all quizzes
     """
 
-    if respondent_id is None:
-        return await crud_quizzes.get_all_quizzes(db=db)
-
-    respondent_quizzes = await crud_quiz_respondent.get_respondent_quizzes(db=db, respondent_id=respondent_id)
-
-    return await crud_quizzes.get_respondent_quizzes(db=db, quizzes_ids=respondent_quizzes)
+    return await crud_quizzes.get_all_quizzes(db=db)
 
 
-@router.get('/{quiz_id}', status_code=status.HTTP_200_OK, response_model=Quiz)
+@router.get('/{quiz_id}', status_code=status.HTTP_200_OK, response_model=RequestedQuiz)
 async def get_quiz_by_id(quiz_id: int, db: AsyncSession = Depends(get_db)) -> Quiz | None:
     """
     Get quiz by id
     """
 
-    quiz: None | Quiz
-    error: None | str
+    if check_quiz_id_is_valid(quiz_id=quiz_id, db=db):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Quiz with this id has does not exist')
 
-    quiz, error = await crud_quizzes.get_quiz_by_id(db, quiz_id=quiz_id)
-    if error is not None:
-        raise HTTPException(status_code=409, detail=error)
-
-    return quiz
+    return crud_quizzes.get_quiz_by_id(quiz_id=quiz_id, db=db)
 
 
 @router.get('/{quiz_id}/respondent/{respondent_id}')
@@ -67,4 +106,4 @@ async def has_access_to_quiz(quiz_id: int, respondent_id: int, db: AsyncSession 
     Check has respondent access to quiz
     """
 
-    return await crud_quizzes.check_access_to_quizz(db=db, quiz_id=quiz_id, respondent_id=respondent_id)
+    return await crud_quiz_respondents.check_access_to_quizz(quiz_id=quiz_id, respondent_id=respondent_id, db=db)
--- backend/src/app/crud/quiz.py
+++ backend/src/app/crud/quiz.py
@@ -11,37 +11,7 @@ class CRUDQuiz:
     def __init__(self, model: Type[Quiz]):
         self.model = model
 
-    async def is_quiz_name_unique(self, db: AsyncSession, quiz_name: str, quiz_id: int = None) -> bool:
-        if quiz_id is None:
-            query = (
-                select(self.model.name)
-                .where(self.model.name == quiz_name.strip())
-            )
-
-            has_quiz_name_added: str = (await db.execute(query)).scalar()
-
-            if has_quiz_name_added is None:
-                return True
-            return False
-        else:
-            query = (
-                select(self.model.id)
-                .where(self.model.name == quiz_name.strip())
-            )
-
-            result_quiz_id: int = (await db.execute(query)).scalar()
-
-            if result_quiz_id != quiz_id:
-                return False
-            return True
-
-    async def create_new_quiz(self, db: AsyncSession, quiz_in: QuizCreate) -> str | None:
-        is_unique_: bool = await self.is_quiz_name_unique(db=db, quiz_name=quiz_in.name)
-
-        # quiz with this name has been already created
-        if not is_unique_:
-            return f'Quiz with this name "{quiz_in.name}" has already been created'
-
+    async def create_new_quiz(self, quiz_in: QuizCreate, db: AsyncSession) -> Quiz:
         new_quiz = Quiz(name=quiz_in.name.strip(),
                         template_id=quiz_in.template_id,
                         description=quiz_in.description
@@ -49,13 +19,9 @@ class CRUDQuiz:
         db.add(new_quiz)
         await db.commit()
 
-    async def update_quiz(self, db: AsyncSession, quiz_id: int, quiz_in: QuizUpdate) -> str | None:
-        is_unique: bool = await self.is_quiz_name_unique(db=db, quiz_name=quiz_in.name, quiz_id=quiz_id)
+        return new_quiz
 
-        # quiz with this name has been already created
-        if not is_unique:
-            return f'Quiz with this name "{quiz_in.name}" has already been created'
-
+    async def update_quiz(self, quiz_id: int, quiz_in: QuizUpdate, db: AsyncSession) -> Quiz:
         query = (
             update(self.model)
             .where(self.model.id == quiz_id)
@@ -63,9 +29,10 @@ class CRUDQuiz:
             .returning(Quiz)
         )
 
-        await db.execute(query)
-
+        updated_quiz: Quiz = (await db.execute(query)).scalar()
         await db.commit()
+
+        return updated_quiz
 
     async def get_all_quizzes(self, db: AsyncSession) -> list[Quiz]:
         query = (
@@ -74,7 +41,7 @@ class CRUDQuiz:
 
         return list((await db.execute(query)).scalars().all())
 
-    async def get_respondent_quizzes(self, db: AsyncSession, quizzes_ids: list[int]) -> list[Quiz]:
+    async def get_respondent_quizzes(self, quizzes_ids: list[int], db: AsyncSession) -> list[Quiz]:
         query = (
             select(self.model)
             .where(self.model.id.in_(quizzes_ids))
@@ -82,16 +49,14 @@ class CRUDQuiz:
 
         return list((await db.execute(query)).scalars().all())
 
-    async def get_quiz_by_id(self, db: AsyncSession, quiz_id: int) -> (Quiz | None, str | None):
+    async def get_quiz_by_id(self, quiz_id: int, db: AsyncSession) -> Quiz:
         query = (
             select(self.model)
             .where(self.model.id == quiz_id)
         )
 
-        quiz = (await db.execute(query)).scalar()
-        if quiz is not None:
-            return quiz, None
-        return None, f'Quiz with this id {quiz_id} does not exist'
+        return (await db.execute(query)).scalar()
+
 
 
 crud = CRUDQuiz(Quiz)
