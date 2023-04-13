2fa62a2b5a9f033413593a9e7a913cb38a3e5ef4
Timur Lidzhiev <trlidzhiev@edu.hse.ru>
2023-04-13 22:51:38 +0300

    Add test token endpoints

---
+++ backend/src/tests/test_token.py
@@ -0,0 +1,24 @@
+from httpx import AsyncClient
+from fastapi import status
+import json
+
+
+async def test_create_new_respondent_and_get_respondent_id(async_client: AsyncClient):
+    # test creating a new respondent
+    response = await async_client.post(url='/api/token/create_respondent')
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+    respondent_token: str = json.loads(response.content)['respondent_token']
+
+    # test getting the respondent id by invalid token
+    response = await async_client.get(url='/api/token/blablabla')
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    assert json.loads(response.content)['detail'] == 'Invalid token'
+
+    # test getting the respondent id by valid token
+    response = await async_client.get(url=f'/api/token/{respondent_token}')
+
+    assert response.status_code == status.HTTP_200_OK
