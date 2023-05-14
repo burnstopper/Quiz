d7c5452fa47a82e7eb5ab78103d3f0edf1259edc
Timur Lidzhiev <trlidzhiev@edu.hse.ru>
2023-05-15 01:59:20 +0300

    add invite link in quiz models and something something else...

--- quiz/backend/app/app/api/templates.py
+++ quiz/backend/app/app/api/templates.py
@@ -8,7 +8,8 @@ from app.models.template import Template
 from app.schemas.template import Template as RequestedTemplate
 from app.schemas.template import TemplateCreate, TemplateUpdate
 from app.utils.validators import check_is_name_unique, check_item_id_is_valid, check_conflicts_with_other_names
-from app.utils.test_data import check_test_id_is_valid, get_requested_template
+from app.utils.test_data import check_test_id_is_valid
+from app.utils.convert_template import get_requested_template
 
 router = APIRouter()
 
