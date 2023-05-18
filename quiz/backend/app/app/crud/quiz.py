dd70063929673af3c32a379a551567760af22ae2
Timur Lidzhiev <trlidzhiev@edu.hse.ru>
2023-05-18 16:09:08 +0300

    change invite link

--- quiz/backend/app/app/crud/quiz.py
+++ quiz/backend/app/app/crud/quiz.py
@@ -1,6 +1,7 @@
 from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.config import settings
 from app.crud.base import CRUDBase
 from app.models.quiz import Quiz
 from app.schemas.quiz import QuizCreate, QuizUpdate
