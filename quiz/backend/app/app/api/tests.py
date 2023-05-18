ce351fe47b9718573fc4d0a4716eb59612cf883c
Timur Lidzhiev <trlidzhiev@edu.hse.ru>
2023-05-18 15:59:35 +0300

    change name func

--- quiz/backend/app/app/api/tests.py
+++ quiz/backend/app/app/api/tests.py
@@ -11,7 +11,7 @@ router = APIRouter()
 
 
 @router.get('/', status_code=status.HTTP_200_OK, response_model=list[TemplateTest])
-async def get_tests_names() -> list[TemplateTest]:
+async def get_tests_data() -> list[TemplateTest]:
     """
     Get all tests
     """
@@ -19,13 +19,3 @@ async def get_tests_names() -> list[TemplateTest]:
     tests_ids: list[int] = [1, 2, 3, 4]  # push back 5 for questionnaire
     return [get_test_data(test_id=test_id) for test_id in tests_ids]
 
-
-@router.get('/', status_code=status.HTTP_200_OK, response_model=list[TemplateTest])
-async def get_template_tests(templates_ids: Annotated[list[int], Query()], db: AsyncSession) -> list[Template]:
-    templates_with_tests: list[Template | None] = [None] * len(templates_ids)
-    for i in range(len(templates_ids)):
-        templates_with_tests[i] = Template(**{'id': templates_ids[i],
-                                              'tests': await get_tests(template_id=templates_ids[i], db=db)
-                                              }
-                                           )
-    return templates_with_tests
