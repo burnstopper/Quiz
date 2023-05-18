a19fae9fefc2cdb39f17005c00365943aa8a0e6c
Timur Lidzhiev <trlidzhiev@edu.hse.ru>
2023-05-18 03:56:00 +0300

    fix tests

--- quiz/backend/app/tests/test_templates.py
+++ quiz/backend/app/tests/test_templates.py
@@ -2,15 +2,12 @@ import json
 
 from fastapi import status
 from httpx import AsyncClient
-from sqlalchemy import select
 
 from app.core.config import settings
-from app.models.quiz import Quiz
-from tests.conftest import async_session_maker
-from tests.test_quizzes import quiz_1, quiz_2, quiz_3
+from tests.test_quizzes import quiz_3
 
 created_template_1 = {
-    'name': 'Created FullTemplate 1',
+    'name': 'Created Template 1',
     'id': 1,
     'tests': [
         {
@@ -33,7 +30,7 @@ created_template_1 = {
 }
 
 created_template_2 = {
-    'name': 'Created FullTemplate 2',
+    'name': 'Created Template 2',
     'id': 2,
     'tests': [
         {
@@ -51,7 +48,7 @@ created_template_2 = {
 }
 
 created_template_3 = {
-    'name': 'Created FullTemplate 3',
+    'name': 'Created Template 3',
     'id': 3,
     'tests': [
         {
@@ -69,7 +66,7 @@ created_template_3 = {
 }
 
 updated_template_1 = {
-    'name': 'Updated FullTemplate 1',
+    'name': 'Updated Template 1',
     'id': 1,
     'tests': [
         {
@@ -92,7 +89,7 @@ updated_template_1 = {
 }
 
 template_1 = {
-    'name': 'Updated FullTemplate 1',
+    'name': 'Updated Template 1',
     'id': 1,
     'tests': [
         {
@@ -123,7 +120,7 @@ async def test_create_template(async_client: AsyncClient):
     # test creating a new template
     tests_ids = [1, 2, 3]
     response = await async_client.post(url='/api/templates/', json={
-        'name': 'Created FullTemplate 1',
+        'name': 'Created Template 1',
         'tests_ids': tests_ids
     })
     assert response.status_code == status.HTTP_201_CREATED
@@ -131,7 +128,7 @@ async def test_create_template(async_client: AsyncClient):
 
     tests_ids: list[int] = [1, 2]
     response = await async_client.post(url='/api/templates/', json={
-        'name': 'Created FullTemplate 2',
+        'name': 'Created Template 2',
         'tests_ids': tests_ids,
     })
     assert response.status_code == status.HTTP_201_CREATED
@@ -139,14 +136,14 @@ async def test_create_template(async_client: AsyncClient):
 
     # test creating a new template with the name that already exists
     response = await async_client.post(url='/api/templates/', json={
-        'name': ' Created FullTemplate 1 ',
+        'name': ' Created Template 1 ',
         'tests_ids': [1, 2, 4]
     })
     assert response.status_code == status.HTTP_409_CONFLICT
 
     tests_ids = [4, 1]
     response = await async_client.post(url='/api/templates/', json={
-        'name': 'Created FullTemplate 3',
+        'name': 'Created Template 3',
         'tests_ids': tests_ids
     })
     assert response.status_code == status.HTTP_201_CREATED
@@ -157,7 +154,7 @@ async def test_update_template(async_client: AsyncClient):
     # test updating the template by id
     updated_first_template_tests_ids = [2, 1, 3]
     response = await async_client.put(url='/api/templates/1', json={
-        'name': 'Updated FullTemplate 1',
+        'name': 'Updated Template 1',
         'tests_ids': updated_first_template_tests_ids
     })
     assert response.status_code == status.HTTP_200_OK
@@ -166,7 +163,7 @@ async def test_update_template(async_client: AsyncClient):
     # tes updating the template that has quizzes
     updated_second_template_tests_ids = [4, 1]
     response = await async_client.put(url='/api/templates/2', json={
-        'name': 'Updated FullTemplate 2',
+        'name': 'Updated Template 2',
         'tests_ids': updated_second_template_tests_ids
     })
     assert response.status_code != status.HTTP_409_CONFLICT
@@ -178,15 +175,15 @@ async def test_update_template(async_client: AsyncClient):
         'tests_ids': [1, 2, 3]
     })
     assert response.status_code == status.HTTP_404_NOT_FOUND
-    assert json.loads(response.content)['detail'] == 'FullTemplate with this id does not exist'
+    assert json.loads(response.content)['detail'] == 'Template with this id does not exist'
 
     # test updating the template with the name that already exists
     response = await async_client.put(url='/api/templates/1', json={
-        'name': ' Updated FullTemplate 2 ',
+        'name': ' Updated Template 2 ',
         'tests_ids': [1, 2]
     })
     assert response.status_code == status.HTTP_409_CONFLICT
-    assert json.loads(response.content)['detail'] == 'FullTemplate with this name has already been created'
+    assert json.loads(response.content)['detail'] == 'Template with this name has already been created'
 
 
 async def test_get_template_by_id(async_client: AsyncClient):
@@ -198,7 +195,7 @@ async def test_get_template_by_id(async_client: AsyncClient):
     # test getting the template by invalid id
     response = await async_client.get(url='/api/templates/4')
     assert response.status_code == status.HTTP_404_NOT_FOUND
-    assert json.loads(response.content)['detail'] == 'FullTemplate with this id does not exist'
+    assert json.loads(response.content)['detail'] == 'Template with this id does not exist'
 
 
 async def test_get_all_templates(async_client: AsyncClient):
